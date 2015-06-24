1f2cb54f29596afea0d12bd76720abdfecd95dcb
Laurent GAY <l.gay@sd-libre.fr>
2015-06-24 16:49:34 +0200

    accounting creation

---
+++ README.rst
@@ -0,0 +1,22 @@
+Diacamma Accounting modules for Lucterios framework
+===================================================
+
+Diacamma Accounting is a Lucterios module to manage accounting.
+
+On our site http://www.lucterios.org, you can find lot of information about Lucterios framework.
+
+If you find any problems or have a new idea to improve this tool, thanks to fill out a ticket here: https://github.com/Lucterios2/contacts/issues
+
+To get more help:
+
+* See our wiki: https://github.com/Lucterios2/core/wiki
+
+To contribute to Lucterios:
+
+* Send a email: contribute@lucterios.org
+
+Featuring of Diacamma Accounting modules
+----------------------------------------
+
+* 
+* Configuration
---
+++ setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+'''
+setup module to pip integration of Diacamma accounting
+
+@author: Laurent GAY
+@organization: sd-libre.fr
+@contact: info@sd-libre.fr
+@copyright: 2015 sd-libre.fr
+@license: This file is part of Lucterios.
+
+Lucterios is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lucterios is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lucterios.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+from setuptools import setup
+from lucterios.contacts import __version__
+
+setup(
+    name="diacamma-accounting",
+    version=__version__,
+    author="Lucterios",
+    author_email="support@lucterios.org",
+    url="http://www.lucterios.org",
+    description="Diacamma accounting managment module for Lucterios framework.",
+    long_description="""
+    Diacamma accounting managment module for Lucterios framework.
+    """,
+    include_package_data=True,
+    platforms=('Any',),
+    license="GNU General Public License v3",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.7',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Natural Language :: French',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database :: Front-Ends',
+    ],
+    packages=["diacamma", "diacamma.accounting"],
+    package_data={
+       "diacamma.accounting.migrations":['*'],
+       "diacamma.accounting":['build', 'images/*', 'locale/*/*/*', 'help/*'],
+    },
+    install_requires=["lucterios >=2.0b0,<2.0b999999999999999", "lucterios-contacts >=2.0b0,<=2.0b999999999999999"],
+)
