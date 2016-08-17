bf55c70a0455f404d236aa2865f79aeae814e269
Laurent GAY <l.gay@sd-libre.fr>
2016-08-17 17:48:09 +0200

    corrections

--- diacamma/payoff/views.py
+++ diacamma/payoff/views.py
@@ -222,10 +222,9 @@ class PayableEmail(XferContainerAcknowledge):
             dlg.add_action(WrapAction(TITLE_CANCEL, 'images/cancel.png'))
         else:
             html_message = "<html>"
-            html_message += message.replace('{[newline]}', '<br/>\n')
+            html_message += message.replace('{[newline]}', '<br/>\n').replace('{[', '<').replace(']}', '>')
             if self.item.payoff_have_payment() and withpayment:
-                html_message += get_html_payment(self.request.META.get('HTTP_REFERER', self.request.build_absolute_uri()),
-                                                 self.language, self.item)
+                html_message += get_html_payment(self.request.META.get('HTTP_REFERER', self.request.build_absolute_uri()), self.language, self.item)
             html_message += "</html>"
             self.item.send_email(subject, html_message, model)
 
