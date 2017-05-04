16a4ec78bc07c9ade47373c1fca3667a715418d0
Laurent GAY <l.gay@sd-libre.fr>
2017-05-04 11:58:58 +0200

    add views

---
+++ diacamma/accounting/views_storage.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+'''
+Describe view for Django
+
+@author: Laurent GAY
+@organization: sd-libre.fr
+@contact: info@sd-libre.fr
+@copyright: 2015 sd-libre.fr
+@license: This file is part of Lucterios.
+
+Lucterios is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lucterios is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lucterios.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+
+from lucterios.framework.tools import MenuManage, FORMTYPE_NOMODAL
+from lucterios.framework.xferadvance import XferListEditor
+
+from diacamma.invoice.models import Bill
+
+
+MenuManage.add_sub("storage", "invoice", "diacamma.invoice/images/invoice.png", _("Storage"), _("Manage of storage"), 10)
+
+
+@MenuManage.describ('invoice.change_bill', FORMTYPE_NOMODAL, 'storage', _('Management of bill list'))
+class BillList(XferListEditor):
+    icon = "bill.png"
+    model = Bill
+    field_id = 'bill'
+    caption = _("Bill")
+
